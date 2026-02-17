aee95df53f0cc076203a9d4ca3aac58a1c47de6e
Sarthak Sinha <s.sarthaksinha99@gmail.com>
2026-02-17 18:32:11 +0530

    more alphabet patterns

---
+++ pattern_printing/alphabetsq.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=65;j<65+n;j++)
+        printf("%c",j);
+        printf("\n");
+    }
+    return 0;
+}
