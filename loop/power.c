661704c7b35f14c709aab223e1da0cdd18ad232c
Sarthak Sinha <s.sarthaksinha99@gmail.com>
2026-02-17 11:48:33 +0530

    power quesrion added in loop

---
+++ loop/power.c
@@ -0,0 +1,17 @@
+// Ques : Two numbers are entered through the
+// keyboard. Write a program to find the value of one
+// number raised to the power of another.
+
+#include<stdio.h>
+int main(){
+    int b,p,result=1;
+    printf("Enter base number : ");
+    scanf("%d",&b);
+    printf("\nEnter power : ");
+    scanf("%d",&p);
+    for(int i=1;i<=p;i++){
+        result = result*b;
+    }
+    printf("%d",result);
+    return 0;
+}
