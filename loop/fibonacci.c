51ed379eb80cc0d119ec22f75adf8db997140bc9
Sarthak Sinha <s.sarthaksinha99@gmail.com>
2026-02-26 22:39:37 +0530

    fibonacci

--- loop/fibonacci.c
+++ loop/fibonacci.c
@@ -1,19 +1,18 @@
-// 1 1 2 3 5 8 13 21 34 55 89....... nth
-// print nth
-
-
 #include<stdio.h>
 int main(){
-    int n;
-    printf("Enter the term number : ");
+    int n,b,a,f;
+    printf("Enter a number : ");
     scanf("%d",&n);
-    int a=1,b=1;
-    int sum =1;
-    for(int i=1;i<=n-2;i++){
+    a = 0;
+    b = 1;
+    
+    for(int i=1;i<=n;i++)
+    {
+        printf("%d ",a);
+        f = a+b;
         a = b;
-        b = sum;
-        sum = a+b;
+        b = f;
+
     }
-    printf("%d",sum);
-    return 0;
+
 }
